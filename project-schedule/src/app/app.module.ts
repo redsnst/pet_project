ebe14a45c127b43f795e1e693d115051be23390c
redsnst <vokibox@hotmail.com>
2023-03-03 10:07:55 +0300

    2 - The homepage, calendar page and navigation between them were implemented

--- project-schedule/src/app/app.module.ts
+++ project-schedule/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { MaterialModule } from './modules/material/material.module';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SelectorComponent } from './selector/selector.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    NavComponentComponent,
+    ScheduleComponent,
+    SelectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
