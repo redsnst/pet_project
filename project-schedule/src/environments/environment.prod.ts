cca1ef8fa0ffe8c6db2f70a9601dc81043838b9a
redsnst <vokibox@hotmail.com>
2023-03-26 22:12:43 +0300

    add gh-pages

--- project-schedule/src/environments/environment.prod.ts
+++ project-schedule/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA1dWvHU_RXbikGabZ6tXgRAy9Gu4IjdBI",
+    authDomain: "my-pet-project-54af1.firebaseapp.com",
+    databaseURL: "https://my-pet-project-54af1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "my-pet-project-54af1",
+    storageBucket: "my-pet-project-54af1.appspot.com",
+    messagingSenderId: "993013374099",
+    appId: "1:993013374099:web:b8abfa1d70e328de42c166"
+  }
 };
